75f927c9819d275c1433fe9985c1a5cd88e6e50e
Horatio Xiao <xiaohoratio@gmail.com>
2020-12-03 16:16:49 -0800

    Added post api for register

--- web/api/src/router.js
+++ web/api/src/router.js
@@ -10,7 +10,7 @@ const bcrypt = require('bcrypt');
 const passport = require('fastify-passport').default;
 const LocalStrategy = require('passport-local');
 
-passport.registerUserSerializer = async function(user, done) {
+passport.registerUserSerializer = async function (user, done) {
     return done(null, user.id);
 }
 
@@ -29,11 +29,11 @@ module.exports = async function (api, opts) {
     // });
 
     passport.use(new LocalStrategy({
-            usernameField: 'email',
-            passwordField: 'password',
-            passReqToCallback: true
-        },
-        function(req, email, password, done) {
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+        function (req, email, password, done) {
             // req.body.email
             // req.body.password
             // req.body.accountType ==> customer, owner, admin
@@ -49,12 +49,12 @@ module.exports = async function (api, opts) {
                         return done(null, false);
                     }
 
-                    user.toArray(function(err, result) {
+                    user.toArray(function (err, result) {
                         if (err || result.length == 0) {
                             // error handling
                             return done(null, false);
                         }
-                        
+
                         if (!bcrypt.compareSync(password, result[0].password)) {
                             return done(null, false);
                         }
@@ -64,63 +64,85 @@ module.exports = async function (api, opts) {
                 });
         }
     ));
-    
+
 
     api.get('/ping', async function (req, res) {
         return { hello: 'colin' };
     });
 
-    api.get('/', { 
+    api.get('/', {
         preValidation: passport.authenticate('local', { successRedirect: '/', failureRedirect: '/auth' })
     }, async function (req, res) {
         return { hi: 'colin' };
     });
 
-    
+
     /**
      * Used to log a user into the platform
      */
-    api.post('/login', async function(req, res) {
-        // TODO
+    api.post('/login', async function (req, res) {
+
     });
 
     /**
      * Used to register a user
      */
-    api.post('/register', async function(req, res) {
-        // TODO
+
+    api.post('/register', async function (req, res) {
+        try {
+            var userType = req.body.userType;
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            var user = {
+                email: req.body.email,
+                username: req.body.username,
+                password: hashedPassword,
+            };
+            console.log(user)
+            return api.db.db("users")
+                .table(userType)
+                .insert(user)
+                .run().then(function (result) {
+                    if (result.errors > 0) {
+                        return { success: false, message: "db insert error" };
+                    }
+                    return { success: true, message: "" };
+                });
+            res.redirect('/login')
+        } catch {
+            res.redirect('/register')
+        }
     });
 
     /**
      * Used to authenticate a user's access to the API
      */
-    api.get('/auth', async function(req, res) {
+    api.get('/auth', async function (req, res) {
         // TODO (this might just be a simple redirect to /login)
     });
 
     /**
      * Used to search for boba restaurants based on a search query
      */
-    api.get('/search/:query', async function(req, res) {
+    api.get('/search/:query', async function (req, res) {
         var query = req.params['query'];
         var str = '^';
         var quer = str.concat(query);
 
         api.db.db("restaurants")
             .table("locations")
-            .filter(function(rest){
+            .filter(function (rest) {
                 return rest('name').match(quer);
             })
             .run().then(function (err, rests) {
-            if (err) {
-                return err;
-            }
-            if(!rests) {
-                return null;
-            }
-        })
+                if (err) {
+                    return err;
+                }
+                if (!rests) {
+                    return null;
+                }
+            })
 
-        var response = {results: []};
+        var response = { results: [] };
         for (let x in rests) {
             response.results.push(x);
         }
@@ -144,13 +166,13 @@ module.exports = async function (api, opts) {
     /**
      * Used to search for boba restaurants relative to the requester's location
      */
-    api.get('/search/:lat/:lng', async function(req, res) {
+    api.get('/search/:lat/:lng', async function (req, res) {
         var lat = req.params['lat'];
         var lng = req.params['lng'];
 
-		// initialize some helper vars
+        // initialize some helper vars
         let radius = 6371e3;
-		var bounds = [];
+        var bounds = [];
         let distance = 20 / 0.6214 * 1000;
         const bearings = [0, 180, 90, 270];
 
@@ -187,23 +209,23 @@ module.exports = async function (api, opts) {
 
         api.db.db("restaurants")
             .table("locations")
-            .between(bounds[0], bounds[1], {rightBound: 'closed'})
+            .between(bounds[0], bounds[1], { rightBound: 'closed' })
             .run().then(function (err, rests) {
                 if (err) {
                     return err;
                 }
-                if(!rests) {
+                if (!rests) {
                     return null;
                 }
-        })
-        var response = {results: []}
+            })
+        var response = { results: [] }
         for (let x in rests) {
-            if (bounds[2] >= x[lon] && bounds[3] <= x[lon] ) {
+            if (bounds[2] >= x[lon] && bounds[3] <= x[lon]) {
                 response.results.push(x)
             }
         }
         return response;
-		
+
         // TODO
         // 1. Determine range of lat/lng coordinates that are in a radius of MAX 25 miles
         // 2. Make a call to the database fetching all entries within the range
@@ -222,7 +244,7 @@ module.exports = async function (api, opts) {
     /**
      * Used to bookmark a location
      */
-    api.put('/bookmark/add/:lat/:lng', async function(req, res) {
+    api.put('/bookmark/add/:lat/:lng', async function (req, res) {
         var lat = req.params['lat'];
         var lng = req.params['lng'];
 
@@ -236,7 +258,7 @@ module.exports = async function (api, opts) {
     /**
      * Used to remove a bookmark
      */
-    api.delete('/bookmark/remove/:lat/:lng', async function(req, res) {
+    api.delete('/bookmark/remove/:lat/:lng', async function (req, res) {
         var lat = req.params['lat'];
         var lng = req.params['lng'];
 
@@ -250,7 +272,7 @@ module.exports = async function (api, opts) {
      * Used to get a restaurant at an exact location
      * - Useful for bookmarks / other requests requiring a lat/lng ==> name translation
      */
-    api.get('/restaurant/:lat/:lng', async function(req, res) {
+    api.get('/restaurant/:lat/:lng', async function (req, res) {
         var lat = req.params['lat'];
         var lng = req.params['lng'];
 
@@ -268,7 +290,7 @@ module.exports = async function (api, opts) {
     /**
      * Used to submit feedback to the restaurant at lat/lng
      */
-    api.post('/feedback/add/:lat/:lng', async function(req, res) {
+    api.post('/feedback/add/:lat/:lng', async function (req, res) {
         var lat = req.params['lat'];
         var lng = req.params['lng'];
 
@@ -280,7 +302,7 @@ module.exports = async function (api, opts) {
     /**
      * Used to get the feedback for a restaurant at lat/lng
      */
-    api.get('/feedback/list/:lat/:lng', async function(req, res) {
+    api.get('/feedback/list/:lat/:lng', async function (req, res) {
         var lat = req.params['lat'];
         var lng = req.params['lng'];
 
