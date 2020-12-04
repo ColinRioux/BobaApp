002e3b1ef0b7e4876bd14f37d0eee67764ee7b37
Horatio Xiao <xiaohoratio@gmail.com>
2020-12-03 16:14:20 -0800

    Added Login page

--- web/frontend/src/LoginView.vue
+++ web/frontend/src/LoginView.vue
@@ -1,23 +1,37 @@
 <template>
     <div id="app">
-        <LoginForm/>
+        <LoginForm class="Right_Half"></LoginForm>
+        <!-- <SearchBar class="Left_Half"></SearchBar> -->
+        <Map class="Left_Half"></Map>
     </div>
 </template>
 
 <script>
-import LoginForm from './components/LoginForm'
+import LoginForm from "./components/LoginForm";
+import Map from "./components/Map";
+import SearchBar from "./components/SearchBar";
 
 export default {
-    name: 'LoginView',
+    name: "LoginView",
     components: {
-        LoginForm
+        LoginForm,
+        Map,
+        // SearchBar,
     },
-    created () {
-        document.title = 'The Social Boba Experience - Login'
+    created() {
+        document.title = "The Social Boba Experience - Login";
     }
-}
+};
 </script>
 
 <style scoped>
+.Left_Half {
+  width: 70%;
+  height: 100%;
+}
 
+.Right_Half {
+  width: 30%;
+  height: 100%;
+}
 </style>
--- web/frontend/src/components/LoginForm.vue
+++ web/frontend/src/components/LoginForm.vue
@@ -1,9 +1,82 @@
 <template>
+  <div>
+    <!-- <div class="left-side">
+      <p>Sign Up</p>
+    </div> -->
     <div class="login-form">
-    
+      <form action="action_page.php" method="post">
+        <div class="container">
+          <label for="uname"><b>Username</b></label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="uname"
+            required
+          />
+
+          <label for="psw"><b>Password</b></label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+          />
+
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <style scoped>
+.login-form {
+  background-color: #ecd5bf;
+  width: 30%;
+  height: 100%;
+  position: absolute;
+  right: 0px;
+  top: 0px;
+}
 
+/* .left-side {
+    background-color: #ceb195;
+    width: 60%;
+    height: 100%;
+    position: absolute;
+    background-repeat: no-repeat;
+  } */
+
+label {
+  color: black;
+}
+
+input {
+  border-radius: 25px;
+  width: 100%;
+  padding: 12px 20px;
+  margin: 9px 0px;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  border-radius: 20px;
+  width: 20%;
+  background-color: #4caf50;
+  color: white;
+  padding: 10px;
+  margin: 9px 10px;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.container {
+  padding: 70px;
+}
 </style>
