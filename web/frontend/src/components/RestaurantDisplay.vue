db6ecc4ff79d5d4a7f5ad1e559225f1f756d6cc2
Colin Rioux <colin350@gmail.com>
2020-12-03 17:00:20 -0700

    use 'modal' like view for feedback & menu

--- web/frontend/src/components/RestaurantDisplay.vue
+++ web/frontend/src/components/RestaurantDisplay.vue
@@ -1,32 +1,77 @@
 <template>
     <div class="restaurant-display">
-        <div class="card-title">
-            <b-button type="is-light" class="card-title-button" @click="goBack">
-                <b-icon pack="fas" icon="backspace"></b-icon>
-            </b-button>
-            <h1 class="title">{{ restaurant.name }}</h1>
+        <div class="control-view">
+            <div class="card-title">
+                <b-button type="is-light" class="card-title-button" @click="goBack">
+                    <b-icon pack="fas" icon="backspace"></b-icon>
+                </b-button>
+                <h1 class="title">{{ restaurant.name }}</h1>
+            </div>
+            <div class="card">
+                <!-- TODO: Stickers -->
+                <h5 class="title is-5">Address</h5>
+                <p class="subtitle is-6">{{ restaurant.address }}</p>
+                <h5 class="title is-5">Hours</h5>
+                <p class="subtitle is-6">
+                    <span v-for="hour in restaurant.hours" :key="hour">{{ hour }}<br/></span>
+                </p>
+                <h5 class="title is-5">Owner</h5>
+                <p class="subtitle is-6 no-bottom">{{ restaurant.owner }}</p>
+                <hr style="margin-bottom: 10px;">
+                <b-button type="is-light" class="card-button" @click="toggleFeedback">
+                    <b-icon pack="fas" icon="comments"></b-icon>
+                </b-button>
+                <!-- TODO: If logged in, see bookmark/edit button -->
+                <b-button type="is-light" class="card-button" @click="gotoBookmark">
+                    <b-icon pack="fas" icon="bookmark"></b-icon>
+                </b-button>
+                <b-button type="is-light" class="card-button" @click="gotoSuggest">
+                    <b-icon pack="fas" icon="pen-square"></b-icon>
+                </b-button>
+                <b-button type="is-light" class="card-button" @click="toggleMenu">
+                    <b-icon pack="fas" icon="book-open"></b-icon>
+                </b-button>
+            </div>
         </div>
-        <div class="card">
-            <!-- TODO: Stickers -->
-            <h5 class="title is-5">Address</h5>
-            <p class="subtitle is-6">{{ restaurant.address }}</p>
-            <h5 class="title is-5">Hours</h5>
-            <p class="subtitle is-6">
-                <span v-for="hour in restaurant.hours" :key="hour">{{ hour }}<br/></span>
-            </p>
-            <h5 class="title is-5">Owner</h5>
-            <p class="subtitle is-6 no-bottom">{{ restaurant.owner }}</p>
-            <hr style="margin-bottom: 10px;">
-            <b-button type="is-light" class="card-button" @click="gotoFeedback">
-                <b-icon pack="fas" icon="comments"></b-icon>
-            </b-button>
-            <!-- TODO: If logged in, see bookmark/edit button -->
-            <b-button type="is-light" class="card-button" @click="gotoBookmark">
-                <b-icon pack="fas" icon="bookmark"></b-icon>
-            </b-button>
-            <b-button type="is-light" class="card-button" @click="gotoSuggest">
-                <b-icon pack="fas" icon="pen-square"></b-icon>
-            </b-button>
+        <div class="menu-view">
+            <div class="card-title">
+                <b-button type="is-light" class="card-title-button" @click="closeMenu">
+                    <b-icon pack="fas" icon="times"></b-icon>
+                </b-button>
+                <h1 class="title" v-if="menuView">Menu</h1>
+                <h1 class="title" v-else>Feedback</h1>
+            </div>
+            <div class="card">
+                <div class="menu-card" v-if="menuView">
+                    <h5 class="title is-5">Menu</h5>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>
+                <div class="feedback-card" v-else>
+                    <form ref="form" class="feedback-form" @submit.prevent="submitFeedback">
+                        <br/>
+                        <b-input 
+                            class="feedback-input" 
+                            v-model="feedbackTitle" 
+                            minlength="5"
+                            maxlength="20"
+                            placeholder="Feedback Title"></b-input>
+                        <b-input 
+                            class="feedback-input" 
+                            v-model="feedbackSummary" 
+                            placeholder="Feedback Summary" 
+                            minlength="10"
+                            maxlength="200" 
+                            type="textarea"></b-input>
+                        <hr style="margin-bottom: 10px; margin-top: 10px; opacity: 0;">
+                        <b-button type="is-light" class="card-button feedback-form-button" @click.prevent="submitFeedback" icon-left="arrow-circle-up">Submit</b-button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -40,6 +85,8 @@ export default {
     components: {},
     methods: {
         getRestaurant(lat, lng) {
+            // Disable menu
+            this.closeMenu();
             axios.get(`http://127.0.0.1:3000/restaurant/get/${lat}/${lng}`)
                 .then((response) => {
                     if (!response.data.success) {
@@ -49,14 +96,57 @@ export default {
                     this.restaurant.hours = this.restaurant.hours.split('\n');
                 }); 
         },
-        gotoFeedback() {
-            this.$router.push({ path: `/feedback?lat=${this.restaurant.lat}&lng=${this.restaurant.lng}`});
+        submitFeedback() {
+            var body = {
+                title: this.feedbackTitle,
+                summary: this.feedbackSummary
+            };
+
+            // Example query url /feedback?lat=35&lng=30
+            // This will be used when we ditch the menu bar feedback button in favor of a feedback link
+            // on the boba restaurant card
+            var lat = (this.$route.query.lat) ? this.$route.query.lat : 37.349667;
+            var lng = (this.$route.query.lng) ? this.$route.query.lng : -121.9477423;
+
+            // Must use arrow function in the "then" to inherit "this"
+            axios.post(`http://127.0.0.1:3000/feedback/add/${lat}/${lng}`, body)
+                .then(response => {
+                    if (!response.data.success) {
+                        // this.submissionResponse = "Your feedback failed to submit! Please try again later!";
+                    } else {
+                        alert("Successfully submitted your feedback!");
+                        // this.exitView();
+                    }
+                });
         },
         gotoBookmark() {
             // TODO
         },
         gotoSuggest() {
 
+        },
+        toggleFeedback() {
+            var menu = document.getElementsByClassName('menu-view')[0];
+            if (this.feedbackView) {
+                menu.style.display = 'none';
+            } else {
+                menu.style.display = 'block';
+            }
+            this.feedbackView = !this.feedbackView;
+            this.menuView = false;
+        },
+        toggleMenu() {
+            var menu = document.getElementsByClassName('menu-view')[0];
+            if (this.menuView) {
+                menu.style.display = 'none';
+            } else {
+                menu.style.display = 'block';
+            }
+            this.menuView = !this.menuView;
+            this.feedbackView = false;
+        },
+        closeMenu() {
+            document.getElementsByClassName('menu-view')[0].style.display = 'none';
         },
         goBack() {
             this.$router.go(-1);
@@ -76,7 +166,9 @@ export default {
     },
     data() {
         return {
-            restaurant: {}
+            restaurant: {},
+            feedbackView: false,
+            menuView: true
         }
     },
     mounted() {
@@ -93,11 +185,29 @@ export default {
     position: absolute;
     top: 3.5em;
     left: 1em;
+    display: block;
+}
+
+.control-view {
     background-color: white;
     padding: 0.5em;
     border-radius: 10px;
-    width: 25%;
     text-align: center;
+    width: 25em;
+    float: left;
+}
+
+.menu-view {
+    background-color: white;
+    padding: 0.5em;
+    padding-left: 0;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    text-align: center;
+    width: 25em;
+    float: right;
+    display: none;
+    margin-top: 1em;
 }
 
 .card {
